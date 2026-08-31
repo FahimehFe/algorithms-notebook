6d9b2bb7af513d973a6047e04df91c8d2bff3a34
Fahimeh Fe <117448210+FahimehFe@users.noreply.github.com>
2026-08-31 13:58:41 +0200

    leetcode problem: Add complexity analysis to longest consecutive sequence
    Added time and space complexity analysis for the problem.

--- solutions/0128-longest-consecutive-sequence.py
+++ solutions/0128-longest-consecutive-sequence.py
@@ -2,6 +2,12 @@
 128. Longest Consecutive Sequence
 https://leetcode.com/problems/longest-consecutive-sequence/
 
+-------------
+
+Time:  O(n) 
+Space: O(n)   - the set
+
+-------------
 """
 from typing import List
 
